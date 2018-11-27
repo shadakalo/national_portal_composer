33bdecf128b089db195f5c55d7b2ee5611cc9e43
nayeem <nayeem.hasanewu@gmail.com>
2018-11-27 11:01:16 +0600

    user table changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('designation');
+            $table->string('mobile',17);
             $table->rememberToken();
             $table->timestamps();
         });
